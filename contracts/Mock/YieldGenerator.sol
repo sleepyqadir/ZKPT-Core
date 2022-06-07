3b20db57f65487da2960193a79beae7f3a690586
AQadir64 <a.qadir67@outlook.com>
2022-06-07 18:40:26 +0500

    add basic yield genertor mock contract

---
+++ contracts/Mock/YieldGenerator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MockYieldGenerator {
+    address public beneficiary;
+
+    constructor (address payable _beneficiary) public {
+       beneficiary = _beneficiary;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == beneficiary);
+        _;
+    }
+//TODO
+//  add the formula for the calculation of yield generated based on pool balance
+    function generateYield(uint[] memory _poolBalance) onlyOwner public {
+            payable(msg.sender).transfer(1 ether);
+    }
+}
