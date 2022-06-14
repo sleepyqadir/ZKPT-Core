f60692f0b1d0b9f588a1dca6c99210464b7a92de
AQadir64 <a.qadir67@outlook.com>
2022-06-14 17:15:11 +0500

    draw completion and winning tickets track functionality

--- contracts/DrawManager.sol
+++ contracts/DrawManager.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "hardhat/console.sol";
+
 contract DrawManager is Ownable {
     using Math for uint256;
 
     // State Variables
     struct DrawStruct {
-        uint256 DrawId;
+        uint256 drawId;
         uint256 startTime;
         uint256 endTime;
         uint256 activeMintingPeriod; // minting tickets is allowed. TASK: rename to "isMintingPeriodActive"?
         bool isCompleted; // winner was found; winnings were deposited.
-        bool isCreated; // is created
     }
 
-    // struct WinningTicketStruct {
-    //     uint256 DrawId;
-    //     uint256 winningTicketIndex;
-    //     bytes32 commitment;
-    // }
+    struct WinningTicketStruct {
+        uint256 drawId;
+        uint256 winningTicketIndex;
+    }
 
     uint256 public constant NUMBER_OF_MINUTES = 10; // 1 week by default; configurable
 
@@ -28,13 +28,11 @@ contract DrawManager is Ownable {
 
     uint256 public numDraws = 0;
 
-    // mapping(uint256 => uint256) public prizes; // key is DrawId
+    mapping(uint256 => uint256) public prizes; // key is DrawId
 
-    // mapping(uint256 => WinningTicketStruct) public winningTickets; // key is DrawId
+    mapping(uint256 => WinningTicketStruct) public winningTickets; // key is DrawId
 
     mapping(uint256 => DrawStruct) public draws; // key is lotteryId
-
-    // mapping(uint256 => mapping(address => uint256)) public pendingWithdrawals; // pending withdrawals for each winner, key is lotteryId, then player address
 
     // Events goes here
     event LogNewLottery(address creator, uint256 startTime, uint256 endTime); // emit when lottery created
@@ -48,22 +46,26 @@ contract DrawManager is Ownable {
     1) minting period manually ended, ie lottery is inactive. Then drawing can begin immediately.
     2) lottery minting period has ended organically, and lottery is still active at that point
     */
-    // modifier isDrawMintingCompleted() {
-    //     if (!((draws[currentDrawId].isActive == true &&
-    //             draws[currentDrawId].endTime < block.timestamp) ||
-    //             draws[currentDrawId].isActive == false)
-    //     ) {
-    //         revert Lottery__MintingNotCompleted();
-    //     }
-    //     _;
-    // }
-
 
     modifier isDrawActive {
       if(draws[currentDrawId].endTime > block.timestamp) {
         revert("Draw: previous draw not ended");
       }
       _;
+    }
+
+    modifier isLotteryMintingCompleted(uint256 drawId) {
+      if(draws[drawId].activeMintingPeriod > block.timestamp) {
+        revert("Draw: minting period is not ended");
+      }
+      _;
+    }
+
+    modifier isCompleted(uint256 drawId) {
+        if(draws[drawId].isCompleted){
+            revert("Draw: draw is already completed");
+        }
+        _;
     }
 
 
@@ -73,7 +75,6 @@ contract DrawManager is Ownable {
      * @title initDraw
      * @dev A function to initialize a new Draw
      * probably should also be onlyOwner
-     * @param uint256 startTime_: start of minting period, unixtime
      * @param uint256 numMinutes: in minutes, how long mint period will last
      */
     function initDraw(uint256 numMinutes_)
@@ -87,62 +88,75 @@ contract DrawManager is Ownable {
             numMinutes_ = NUMBER_OF_MINUTES;
         }
         uint256 endTime = block.timestamp + (numMinutes_ * 1 minutes);
+        uint256 activeMintingPeriod = block.timestamp + 1 minutes;
         draws[currentDrawId] = DrawStruct({
-            DrawId: currentDrawId,
+            drawId: currentDrawId,
             startTime: block.timestamp,
             endTime: endTime,
-            activeMintingPeriod: 1 minutes,
-            isCompleted: false,
-            isCreated: true
+            activeMintingPeriod: activeMintingPeriod,
+            isCompleted: false
         });
-        numDraws = numDraws + 1;
+        numDraws++;
+        currentDrawId++;
         emit LogNewLottery(msg.sender, block.timestamp, endTime);
     }
 
-    // /*
-    //  * @title triggerLotteryDrawing
-    //  * @dev a function for owner to trigger lottery drawing
-    //  */
-    // function triggerDrawing()
-    //     external
-    //     isLotteryMintingCompleted
-    //     onlyOwner
-    // {
-    //     // console.log("triggerLotteryDrawing");
-    //     prizes[currentDrawId] = prizeAmount; // keep track of prize amts for each of the previous lotteries
+    /*
+     * @title trigger draw completed will select the random user from the merkle root history using vrf function
+     * @dev a function for owner to trigger lottery drawing
+     */
+    function triggerDrawComplete(uint256 drawId)
+        external
+        isLotteryMintingCompleted(drawId)
+        isCompleted(drawId)
+        onlyOwner
+    {
+        draws[drawId].isCompleted = true;
+        uint256 random = rand();
 
-    //     _playerTicketDistribution(); // create the distribution to get ticket indexes for each user
-    //     // can't be done a priori bc of potential multiple mints per user
-    //     uint256 winningTicketIndex = _performRandomizedDrawing();
-    //     // initialize what we can first
-    //     winningTicket.currentLotteryId = currentLotteryId;
-    //     winningTicket.winningTicketIndex = winningTicketIndex;
-    //     findWinningAddress(winningTicketIndex); // via binary search
+        winningTickets[drawId] = WinningTicketStruct({
+          drawId: drawId,
+          winningTicketIndex: random
+        });
+    }
 
-    //     emit LogWinnerFound(
-    //         currentLotteryId,
-    //         winningTicket.winningTicketIndex,
-    //         winningTicket.addr
-    //     );
-    // }
 
-    // /*
-    //  * @title triggerDepositWinnings // TASK: rename to maybe depositWinnings
-    //  * @dev function to deposit winnings for user withdrawal pattern
-    //  * then reset lottery params for new one to be created
-    //  */
-    // function triggerDepositWinnings() public {
-    //     // console.log("triggerDepositWinnings");
-    //     pendingWithdrawals[currentLotteryId][winningTicket.addr] = prizeAmount;
-    //     prizeAmount = 0;
-    //     lotteries[currentLotteryId].isCompleted = true;
-    //     winningTickets[currentLotteryId] = winningTicket;
-    //     // emit before resetting lottery so vars still valid
-    //     emit LotteryWinningsDeposited(
-    //         currentLotteryId,
-    //         winningTicket.addr,
-    //         pendingWithdrawals[currentLotteryId][winningTicket.addr]
-    //     );
-    // }
+function vrf() internal view returns (bytes32 result) {
+    uint[1] memory bn;
+    bn[0] = block.number;
+    assembly {
+      let memPtr := mload(0x40)
+      if iszero(staticcall(not(0), 0xff, bn, 0x20, memPtr, 0x20)) {
+        invalid()
+      }
+      result := mload(memPtr)
+    }
+  }
+
+   
+    function rand()
+    public
+    view
+    returns(uint256)
+{
+    uint256 seed = uint256(keccak256(abi.encodePacked(
+        block.timestamp + block.difficulty +
+        ((uint256(keccak256(abi.encodePacked(block.coinbase)))) / (block.timestamp)) +
+        block.gaslimit + 
+        ((uint256(keccak256(abi.encodePacked(msg.sender)))) / (block.timestamp)) +
+        block.number
+    )));
+
+    return (seed - ((seed / 1000) * 1000));
+}
+
+    /*
+     * @title triggerDepositWinnings // TASK: rename to maybe depositWinnings
+     * @dev function to deposit winnings for user withdrawal pattern
+     * then reset lottery params for new one to be created
+     */
+    function _triggerDrawWinnings() public {
+        // emit before resetting lottery so vars still valid
+    }
 
 }
