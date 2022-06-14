51ed397fb38734fad0f3c5f85c7f0bcebfab5bdc
AQadir64 <a.qadir67@outlook.com>
2022-06-14 17:18:58 +0500

    #DrawManager and draw test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -32,6 +32,10 @@ chai.use(solidity);
 
 const ETH_AMOUNT = ethers.utils.parseEther("1");
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 describe("ZkPoolTogether", async () => {
   // TODO change the any type to the typechain
   let merkleTreeWithHistory: any;
@@ -65,102 +69,102 @@ describe("ZkPoolTogether", async () => {
     );
   });
 
-  // describe("Poseidon", async () => {
-  //   it("generates same poseidon hash", async function () {
-  //     const res = await poseidonContract["poseidon(uint256[2])"]([1, 2]);
-  //     const res2 = poseidon([1, 2]);
-  //     expect(res.toString()).equals(poseidon.F.toString(res2));
-  //   }).timeout(500000);
-  // });
+  describe("Poseidon", async () => {
+    it("generates same poseidon hash", async function () {
+      const res = await poseidonContract["poseidon(uint256[2])"]([1, 2]);
+      const res2 = poseidon([1, 2]);
+      expect(res.toString()).equals(poseidon.F.toString(res2));
+    }).timeout(500000);
+  });
 
-  // describe("MerkleTreeWithHistory", function () {
-  //   describe("#contructor", () => {
-  //     it("should initialize", async () => {
-  //       const zeroValue = await merkleTreeWithHistory.ZERO_VALUE();
-  //       const firstSubtree = await merkleTreeWithHistory.filledSubtrees(0);
-  //       expect(firstSubtree).equals(zeroValue);
-  //       const firstZero = await merkleTreeWithHistory.zeros(0);
-  //       expect(firstZero).equals(zeroValue);
-  //     });
-  //   });
+  describe("MerkleTreeWithHistory", function () {
+    describe("#contructor", () => {
+      it("should initialize", async () => {
+        const zeroValue = await merkleTreeWithHistory.ZERO_VALUE();
+        const firstSubtree = await merkleTreeWithHistory.filledSubtrees(0);
+        expect(firstSubtree).equals(zeroValue);
+        const firstZero = await merkleTreeWithHistory.zeros(0);
+        expect(firstZero).equals(zeroValue);
+      });
+    });
 
-  //   describe("#insert", () => {
-  //     it("should insert", async () => {
-  //       let lastRoot;
-  //       for (let i = 1; i < 5; i++) {
-  //         await merkleTreeWithHistory.insert(toFixedHex(i));
-  //         lastRoot = await merkleTreeWithHistory.getLastRoot();
-  //       }
-  //       const knownRoot = await merkleTreeWithHistory.isKnownRoot(lastRoot);
-  //       // eslint-disable-next-line no-unused-expressions
-  //       expect(knownRoot).to.be.true;
-  //     });
-  //   });
-  // });
+    describe("#insert", () => {
+      it("should insert", async () => {
+        let lastRoot;
+        for (let i = 1; i < 5; i++) {
+          await merkleTreeWithHistory.insert(toFixedHex(i));
+          lastRoot = await merkleTreeWithHistory.getLastRoot();
+        }
+        const knownRoot = await merkleTreeWithHistory.isKnownRoot(lastRoot);
+        // eslint-disable-next-line no-unused-expressions
+        expect(knownRoot).to.be.true;
+      });
+    });
+  });
 
   describe("Pool", async () => {
-    // it("#Deposit and withdraw", async () => {
-    //   const [depositor, relayer, withdrawer] = await ethers.getSigners();
-    //   const deposit = Deposit.new(poseidon);
-    //   const commitment = deposit.commitment;
-    //   const depositTransaction = await ETHZkPool.connect(depositor).deposit(
-    //     commitment,
-    //     {
-    //       value: ETH_AMOUNT,
-    //     }
-    //   );
-    //   const depositReciept = await depositTransaction.wait();
+    it("#Deposit and withdraw", async () => {
+      const [depositor, relayer, withdrawer] = await ethers.getSigners();
+      const deposit = Deposit.new(poseidon);
+      const commitment = deposit.commitment;
+      const depositTransaction = await ETHZkPool.connect(depositor).deposit(
+        commitment,
+        {
+          value: ETH_AMOUNT,
+        }
+      );
+      const depositReciept = await depositTransaction.wait();
 
-    //   const events = await ETHZkPool.queryFilter(
-    //     ETHZkPool.filters.Deposit(),
-    //     depositReciept.blockHash
-    //   );
+      const events = await ETHZkPool.queryFilter(
+        ETHZkPool.filters.Deposit(),
+        depositReciept.blockHash
+      );
 
-    //   assert.equal(events[0].args.commitment, commitment);
-    //   deposit.leafIndex = events[0].args.leafIndex;
+      assert.equal(events[0].args.commitment, commitment);
+      deposit.leafIndex = events[0].args.leafIndex;
 
-    //   const tree = new MerkleTree(20, "test", new PoseidonHasher(poseidon));
-    //   assert.equal(await tree.root(), await ETHZkPool.roots(0));
-    //   await tree.insert(deposit.commitment);
-    //   assert.equal(tree.totalElements, await ETHZkPool.nextIndex());
-    //   assert.equal(await tree.root(), await ETHZkPool.roots(1));
+      const tree = new MerkleTree(20, "test", new PoseidonHasher(poseidon));
+      assert.equal(await tree.root(), await ETHZkPool.roots(0));
+      await tree.insert(deposit.commitment);
+      assert.equal(tree.totalElements, await ETHZkPool.nextIndex());
+      assert.equal(await tree.root(), await ETHZkPool.roots(1));
 
-    //   const nullifierHash = deposit.nullifierHash;
-    //   const recipient = await withdrawer.getAddress();
-    //   const fee = 0;
+      const nullifierHash = deposit.nullifierHash;
+      const recipient = await withdrawer.getAddress();
+      const fee = 0;
 
-    //   const { root, path_elements, path_index } = await tree.path(
-    //     deposit.leafIndex
-    //   );
+      const { root, path_elements, path_index } = await tree.path(
+        deposit.leafIndex
+      );
 
-    //   const witness = {
-    //     // Public
-    //     root,
-    //     nullifierHash,
-    //     recipient,
-    //     relayer: await relayer.getAddress(),
-    //     fee,
-    //     secret: 1,
-    //     // Private
-    //     nullifier: BigNumber.from(deposit.nullifier).toBigInt(),
-    //     pathElements: path_elements,
-    //     pathIndices: path_index,
-    //   };
+      const witness = {
+        // Public
+        root,
+        nullifierHash,
+        recipient,
+        relayer: await relayer.getAddress(),
+        fee,
+        secret: 1,
+        // Private
+        nullifier: BigNumber.from(deposit.nullifier).toBigInt(),
+        pathElements: path_elements,
+        pathIndices: path_index,
+      };
 
-    //   const solidityProof = await prove(witness);
+      const solidityProof = await prove(witness);
 
-    //   console.log({ witness, solidityProof });
-    //   const txWithdraw = await ETHZkPool.connect(relayer).withdraw(
-    //     solidityProof,
-    //     root,
-    //     nullifierHash,
-    //     recipient,
-    //     await relayer.getAddress(),
-    //     fee
-    //   );
-    //   const receiptWithdraw = await txWithdraw.wait();
-    //   console.log("Withdraw gas cost", receiptWithdraw.gasUsed.toNumber());
-    // });
+      console.log({ witness, solidityProof });
+      const txWithdraw = await ETHZkPool.connect(relayer).withdraw(
+        solidityProof,
+        root,
+        nullifierHash,
+        recipient,
+        await relayer.getAddress(),
+        fee
+      );
+      const receiptWithdraw = await txWithdraw.wait();
+      console.log("Withdraw gas cost", receiptWithdraw.gasUsed.toNumber());
+    });
 
     it("#Start A new Draw", async () => {
       const [signer] = await ethers.getSigners();
@@ -184,11 +188,45 @@ describe("ZkPoolTogether", async () => {
     });
     it("#Get The End and Start Time of the Draw", async () => {
       const [signer] = await ethers.getSigners();
-      const transaction = await ETHZkPool.connect(signer).initDraw(3);
+      const transaction = await ETHZkPool.connect(signer).initDraw(1);
       const reciept = transaction.wait();
 
       const draw1 = await ETHZkPool.draws(0);
-      console.log({ draw1 });
+      await sleep(60 * 1000);
+      const secondTransaction = await ETHZkPool.connect(signer).initDraw(3);
+      const secondReciept = await secondTransaction.wait();
+      const draw2 = await ETHZkPool.draws(1);
+      console.log({ draw1, draw2 });
+    }).timeout(2 * 60 * 1000);
+    it("#Should fail to trigger the triggerDrawComplete before 1 min", async () => {
+      const [signer] = await ethers.getSigners();
+      const transaction = await ETHZkPool.connect(signer).initDraw(3);
+      const reciept = transaction.wait();
+      const draw1 = await ETHZkPool.draws(0);
+      console.log(draw1);
+      await expect(
+        ETHZkPool.connect(signer).triggerDrawComplete(draw1.drawId)
+      ).to.be.revertedWith("Draw: minting period is not ended");
     });
+    it("#Should allow to trigger the triggerDrawComplete after 1 min", async () => {
+      const [signer] = await ethers.getSigners();
+      const transaction = await ETHZkPool.connect(signer).initDraw(3);
+      const reciept = transaction.wait();
+      const draw1 = await ETHZkPool.draws(0);
+      await sleep(60 * 1000);
+      const triggerCompleteTransaction = await ETHZkPool.connect(
+        signer
+      ).triggerDrawComplete(draw1.drawId);
+
+      triggerCompleteTransaction.wait();
+      const drawAfterComplete = await ETHZkPool.draws(0);
+      // eslint-disable-next-line no-unused-expressions
+      expect(drawAfterComplete.isCompleted).to.be.true;
+      expect(await (await ETHZkPool.winningTickets(0)).drawId).equals(0);
+      await expect(
+        ETHZkPool.connect(signer).triggerDrawComplete(draw1.drawId)
+      ).to.be.revertedWith("Draw: draw is already completed");
+      console.log({ ticket: await ETHZkPool.winningTickets(0) });
+    }).timeout(1.5 * 60 * 1000);
   });
 });
